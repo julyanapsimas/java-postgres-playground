71d6b1b8dfbd7f7f5ac705a2f3c9070d0bbb17dd
Julyana Pereira Simas Bassakin <julyana.psimas@gmail.com>
2023-08-21 14:23:27 +0000

    teste primeiro commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,6 +10,11 @@ public class App {
     private static final String JDBC_URL = "jdbc:postgresql://localhost/postgres";
 
     public static void main(String[] args) {
+
+        System.out.println(" ");
+
+        System.out.println("Iniciando...");
+
         new App();
     }
 
